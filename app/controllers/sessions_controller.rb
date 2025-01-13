2f9a60b103c2e7c01767e18c281336ebdbc71485
dak2 <dawt2h@gmail.com>
2025-01-13 13:08:50 +0000

    redirect to root if user logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
+  before_action :redirect_to_root_if_logged_in, only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
@@ -36,5 +37,9 @@ class SessionsController < ApplicationController
   private
     def auth_hash
       request.env['omniauth.auth']
+    end
+
+    def redirect_to_root_if_logged_in
+      redirect_to after_authentication_url if authenticated?
     end
 end
